948afd170b9dc185255afa8dd5bfa57d72ffcad2
Jeremiah <jeremiah.fallin@gmail.com>
2023-04-09 22:13:47 -0700

    types

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -3,12 +3,10 @@
  */
 import { publicProcedure, router } from '../trpc';
 import { leagueRouter } from './league';
-import { postRouter } from './post';
 
 export const appRouter = router({
   healthcheck: publicProcedure.query(() => 'yay!'),
 
-  post: postRouter,
   league: leagueRouter,
 });
 
