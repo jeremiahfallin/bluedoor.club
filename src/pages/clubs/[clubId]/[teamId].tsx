9abee1d591c089b7bcf3b0f1dd31c915d37c1815
Jeremiah <jeremiah.fallin@gmail.com>
2023-06-01 20:45:10 -0700

    update old trpc usage

--- src/pages/clubs/[clubId]/[teamId].tsx
+++ src/pages/clubs/[clubId]/[teamId].tsx
@@ -7,39 +7,40 @@ const ClubTeamPage = () => {
   const router = useRouter();
   const { clubId, teamId } = router.query;
 
-  const teamQuery = useQuery(
-    ['club', clubId, 'team', teamId],
-    async () => {
-      if (!clubId || !teamId) return;
-      const team = await trpc.query('club.getTeamById', {
-        clubId: Number(clubId),
-        teamId: Number(teamId),
-      });
-      return team;
-    },
-    {
-      enabled: !!clubId && !!teamId,
-    },
-  );
+  // const teamQuery = useQuery(
+  //   ['club', clubId, 'team', teamId],
+  //   async () => {
+  //     if (!clubId || !teamId) return;
+  //     const team = await trpc.query('club.getTeamById', {
+  //       clubId: Number(clubId),
+  //       teamId: Number(teamId),
+  //     });
+  //     return team;
+  //   },
+  //   {
+  //     enabled: !!clubId && !!teamId,
+  //   },
+  // );
 
   if (!clubId || !teamId) return null;
+  return null;
 
-  return (
-    <VStack spacing={4}>
-      {teamQuery.isLoading && <Text>Loading team...</Text>}
-      {teamQuery.error && (
-        <Text>Error loading team: {teamQuery.error.message}</Text>
-      )}
-      {teamQuery.data && (
-        <>
-          <Heading as="h1" size="2xl">
-            Team: {teamQuery.data.name}
-          </Heading>
-          <Text>Club: {teamQuery.data.club.name}</Text>
-        </>
-      )}
-    </VStack>
-  );
+  // return (
+  //   <VStack spacing={4}>
+  //     {teamQuery.isLoading && <Text>Loading team...</Text>}
+  //     {teamQuery.error && (
+  //       <Text>Error loading team: {teamQuery.error.message}</Text>
+  //     )}
+  //     {teamQuery.data && (
+  //       <>
+  //         <Heading as="h1" size="2xl">
+  //           Team: {teamQuery.data.name}
+  //         </Heading>
+  //         <Text>Club: {teamQuery.data.club.name}</Text>
+  //       </>
+  //     )}
+  //   </VStack>
+  // );
 };
 
 export default ClubTeamPage;
